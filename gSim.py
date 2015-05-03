6b14a05a5158dfbbb277a15d2e8e20dabe94b224
Henrique Nogueira <henrique.goncalves.nogueira@gmail.com>
2015-05-04 00:05:48 +0100

    Replaced body with new class gSim, increased organization, new_body_click function in main

---
+++ gSim.py
@@ -0,0 +1,63 @@
+"""In this python module we try to simulate the behaviour of free moving bodies interacting only through gravitational force. It is composed by:
+  -Class body, that simply stores information regarding a specific body with mass
+  -Functions that calculate the next position of a list(body1,body2,...) given their current status
+
+  G is the gravitational constant
+"""
+G = 6.67*10**-11
+
+
+class body(object):
+    def __init__(self, mass, radius, color, position, speed):
+        self.mass = mass         #type int
+        self.radius = radius     #type int
+        self.color = color       #type list(int,int,int) RGB
+        self.position = position #type list(float x, float y, float z)
+        self.speed = speed       #type list(float x, float y, float z)
+      
+
+    
+def nextVel_with_steps(bodies, step):#already called by nextpos
+    """ This functions takes a list of multiple type body changes their velocities to the next iterative value"""
+    
+    for it in range(len(bodies)):#double iteration
+        for idx in range(len(bodies)):# for speed replacement
+            if it == idx: continue 
+            r = get_distance(bodies[it], bodies[idx])
+            r = ((bodies[it].position[0] - bodies[idx].position[0])**2 + (bodies[it].position[1] - bodies[idx].position[1])**2 + (bodies[it].position[2] - bodies[idx].position[2])**2)**(1./2.) #distance between 2 bodies
+            if r!= 0:
+               bodies[it].speed[0] += -step*G*(bodies[idx].mass)*(bodies[it].position[0] - bodies[idx].position[0])/r**2
+               bodies[it].speed[1] += -step*G*(bodies[idx].mass)*(bodies[it].position[1] - bodies[idx].position[1])/r**2
+               bodies[it].speed[2] += -step*G*(bodies[idx].mass)*(bodies[it].position[2] - bodies[idx].position[2])/r**2
+            idx+=1
+        it+=1
+            
+def next_pos_with_steps(bodies, step):
+    """This function calculates the next iterative position using steps computing, it automatically calls for the function nextVel_with_steps """
+    nextVel_with_steps(bodies, step)
+    for body in bodies:
+        body.position[0]+=step*body.speed[0]
+        body.position[1]+=step*body.speed[1]
+        body.position[2]+=step*body.speed[2]
+
+
+def get_centerOfMass(bodies):
+    """This Function returns the center of mass of a given list of body's"""
+
+    posCM = [0,0,0]
+    mTotal = 0
+    for body in bodies:
+        posCM[0]+=body.position[0]*body.mass
+        posCM[1]+=body.position[1]*body.mass
+        posCM[2]+=body.position[2]*body.mass
+        mTotal+=body.mass
+    
+    posCM[0]=posCM[0]/mTotal
+    posCM[1]=posCM[1]/mTotal
+    posCM[2]=posCM[1]/mTotal
+
+    return posCM
+
+def get_distance(body1, body2):
+    r = ((body1.position[0] - body2.position[0])**2 + (body1.position[1] - body2.position[1])**2 + (body1.position[2] - body2.position[2])**2)**(1./2.)
+    return r
--- main.py
+++ main.py
@@ -1,60 +1,64 @@
-import sys, pygame, random, body
+import pygame, gSim, sys
 from pygame.locals import *
 
+""" Main script with all the drawing methods and main loop"""
 
-def draw_next(objects, screen):
-    idx = 0
-    for object in objects:
-        object.position = body.next_pos(object)
-        pygame.draw.circle(screen,object.color,[int(object.position[0]),int(object.position[1])],object.radius)
+G = 6.67*10**-11
+
+def draw_next_with_steps(bodies, step,screen):
+    gSim.next_pos_with_steps(bodies, step)
+    for body in bodies:
+        pygame.draw.circle(screen,body.color,[int(body.position[0]),int(body.position[1])],body.radius)
         
-        object.speed = body.eval_nextVel(objects, idx)
-        idx += 1
 
-def draw_centerOfMass(objects, screen):
-    idx = 0 
-    posCM = [0,0,0]
-    mTotal = 0
-    for object in objects:
-        posCM[0]+=object.position[0]*object.mass
-        posCM[1]+=object.position[1]*object.mass
-        posCM[2]+=object.position[2]*object.mass
-        mTotal+=object.mass
-    
+def draw_centerOfMass(bodies, screen):
+    posCM = gSim.get_centerOfMass(bodies)
+    pygame.draw.circle(screen,(255,255,255),(int(posCM[0]),int(posCM[1])),1)
 
-
-    posCM[0]=posCM[0]/mTotal
-    posCM[1]=posCM[1]/mTotal
-    
-    #print posCM
-    
-    pygame.draw.circle(screen, (0,0,0), [int(posCM[0]), int(posCM[1])], 1)
-
-def totalEnergy(objects):
-    G=1
-    total_energy = 0
+def print_totalEnergy(bodies,screen, f, iteration):
     kinetic = 0
-
-    for body in objects:
-        kinetic+= 1./2.*float(body.mass)*(body.speed[0]**2+body.speed[1]**2+body.speed[2]**2)
-
-    
     potential = 0
-    for idx in range(len(objects)):
-        for i in range(len(objects)):
-            if(i  == idx): continue
-            r=((objects[idx].position[0] - objects[i].position[0])**2 + (objects[idx].position[1] - objects[i].position[1])**2 + (objects[idx].position[2] - objects[i].position[2])**2)**(1./2.)
-            if r!= 0:
-                potential-=objects[i].mass*objects[idx].mass*G/r
-    
 
-    print "Kinetic = " + str(10000*kinetic)
-    print "Potential= " + str(10000*potential)
-    print "Total Energie = " + str(10000*kinetic+10000*potential)
+    for body in bodies:#calc kinetic energy
+        kinetic+=body.mass*1./2.*float(body.mass)*(body.speed[0]**2+body.speed[1]**2+body.speed[2]**2)
+    print "Kinetic = " + str(kinetic)
+    
+    for i in range(len(bodies)):
+        for it in range(len(bodies)):
+            if i == it: continue
+            r = gSim.get_distance(bodies[i], bodies[it])
+            potential-=G*bodies[i].mass*bodies[it].mass/r
+
+    print "Potential = " + str(potential)
+    totalEnergy =potential + kinetic
+    print "Total = " + str(totalEnergy)      
+    f.write(str(iteration) + " " + str(kinetic) + " " + str(potential) + " " + str(totalEnergy) + "\n")
+
+def new_body_click(bodies):
+    mouseDown = pygame.mouse.get_pos()
+    mouseUp = []
+    wait = True
+    while wait:
+        for event in pygame.event.get(): 
+            if event.type == MOUSEBUTTONUP:
+                mouseUp = pygame.mouse.get_pos()
+                wait = False
+                break
+                
+    newSpeed = [0,0]
+    newSpeed[0] = float(10*(mouseUp[0]-mouseDown[0]))
+    newSpeed[1] = float(10*(mouseUp[1]-mouseDown[1]))
+    print newSpeed
+    
+    newbody = gSim.body(6*10**2,1,(0,255,0),[mouseDown[0],mouseDown[1],0.],[newSpeed[0],newSpeed[1],0.])
+    bodies.append(newbody)
+
 
 def main():
     pygame.init()
-
+    f = open("data.txt", "w")
+    f.write("#iteration || Kinetic || Potential || Mechanical \n")
+    iteration = 0
     RATIO = 9./12.
     WIDTH = 700
     HEIGHT = int(WIDTH*RATIO)
@@ -62,23 +66,23 @@ def main():
     screen = pygame.display.set_mode([WIDTH,HEIGHT])
     CLOCK = pygame.time.Clock()
 
-    body0 = body.object(1000,7,(255,0,0),[400,200,0],[0,-1000,0])
-    body1 = body.object(1000,7,(0,255,0),[200,200,0],[0,1000,0])
-    body2 = body.object(0.01,7,(0,0,255),[300,200,0],[0,0,0])
+    body0 = gSim.body(6*10**16,10,(255,0,0),[380.,200.,0.],[0.,000.,0.])
+    body1 = gSim.body(6*10**4,1,(255,123,187),[400.,200.,0.],[0.,3000.,0.])
 
-    bodies =[body1, body2, body0]
+    bodies =[body0, body1]
     while True:
         for event in pygame.event.get():
             #print str(event)
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                new_body_click(bodies)
 
-        screen.fill((255,255,255))
-        draw_next(bodies, screen)
+        draw_next_with_steps(bodies, 0.0001, screen)
         draw_centerOfMass(bodies,screen)
-        totalEnergy(bodies)
-        
-        pygame.display.flip()
-        CLOCK.tick(60)
+        print_totalEnergy(bodies,screen, f, iteration)
 
+        pygame.display.flip()
+        CLOCK.tick(30)
+        iteration +=1
 main()
